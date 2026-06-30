84b2d7fbb36a61fd8eade682f0929c993e7c6d5d
Anderson Haru Arai Matsunaka <anderson.haruo20@gmail.com>
2026-06-30 16:51:18 -0400

    Ajuste nos textos

--- src/main/java/br/ucalc/calculadora_pcs/model/enums/TipoEmenda.java
+++ src/main/java/br/ucalc/calculadora_pcs/model/enums/TipoEmenda.java
@@ -3,8 +3,8 @@ package br.ucalc.calculadora_pcs.model.enums;
 public enum TipoEmenda {
 
     NENHUMA("Nenhuma"),
-    EC113("Selic (EC 113/2021)"),
-    EC136("Selic até 09/09/2025 → IPCA + 2% (EC 136/2025)");
+    EC113("SELIC (EC 113/2021)"),
+    EC136("SELIC até 09/09/2025 → IPCA + 2% (EC 136/2025)");
 
     private final String descricao;
 
